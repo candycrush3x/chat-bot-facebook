e2a494b6fae179e5b82b1d893cad77dce3548c08
WhoAmI1x <trantrunghuynh1998@gmail.com>
2023-06-07 22:39:50 +0700

    Retry

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -10,9 +10,16 @@ const postWebhook = (req, res) => {
 
   if (body.object === "page") {
     body.entry.forEach((entry) => {
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      let sender_psid = webhook_event.sender.id;
+      console.log(webhook_event);
 
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
@@ -42,14 +49,19 @@ const getMessageWebhook = (req, res) => {
   }
 };
 
+// Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
 
+  // Checks if the message contains text
   if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
     response = {
-      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
     };
   } else if (received_message.attachments) {
+    // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
       attachment: {
@@ -80,30 +92,38 @@ function handleMessage(sender_psid, received_message) {
     };
   }
 
+  // Send the response message
   callSendAPI(sender_psid, response);
 }
 
+// Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
   let response;
 
-  const payload = received_postback.payload;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
 
+  // Set the response based on the postback payload
   if (payload === "yes") {
     response = { text: "Thanks!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
   }
+  // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
 
+// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  const request_body = {
+  // Construct the message body
+  let request_body = {
     recipient: {
       id: sender_psid,
     },
     message: response,
   };
 
+  // Send the HTTP request to the Messenger Platform
   request(
     {
       uri: "https://graph.facebook.com/v2.6/me/messages",
