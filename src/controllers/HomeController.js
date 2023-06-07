a9cdeb3e64f40889dab1c5f36dddae0d051af43b
WhoAmI1x <trantrunghuynh1998@gmail.com>
2023-06-07 22:24:36 +0700

    Test log

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -106,7 +106,7 @@ function callSendAPI(sender_psid, response) {
 
   request(
     {
-      uri: "https://graph.facebook.com/v17.0/me/messages",
+      uri: "https://graph.facebook.com/v2.6/me/messages",
       qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
       method: "POST",
       json: request_body,
